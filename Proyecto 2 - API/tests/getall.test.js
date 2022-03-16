12727d66492f4f8bba26095f1e4d067587c9627d
Vieris <alex12293@gmail.com>
2022-03-16 11:43:15 +0100

    update jest-test

--- Proyecto 2 - API/tests/getall.test.js
+++ Proyecto 2 - API/tests/getall.test.js
@@ -1,14 +1,17 @@
 const request = require('supertest')
 const app = require('../index.js')
-describe('Get All', () => {
-  it('should send json', () => {
-    return request(app)
-      .get("/get")
-      .expect(200);
-    //expect(res.body).toHaveProperty('post')
-  })
-})
 
 afterAll(done => {
-  done()
+  app.close();
+  done();
+});
+
+describe('Get All', () => {
+  it('should send json', async () => {
+    const res = await request(app)
+      .get("/get")
+    expect(200)
+    expect(res.body && typeof res.body === 'object').toBe(true)
+    expect(res.body).toHaveProperty('empleados');
+  })
 })
