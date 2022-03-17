751033a176a660a5b47c630da1d13f1b30bfc8e5
Carlos Jesús Pérez Márquez <permarcar96@gmail.com>
2022-03-17 13:25:03 +0100

    Scenario 4

--- cucumber/src/test/java/Proyecto2/cucumber/StepDefinitions.java
+++ cucumber/src/test/java/Proyecto2/cucumber/StepDefinitions.java
@@ -17,104 +17,119 @@ import java.util.List;
 import org.json.JSONObject;
 import org.junit.Assert;
 
+import groovyjarjarantlr4.v4.parse.ANTLRParser.exceptionGroup_return;
+
 public class StepDefinitions {
 
-	private static final int PORT = 8080;
-	RequestSpecification request;
-	
-	//Feature: Consultar todos los empleados
-    
+    private static final int PORT = 8080;
+    RequestSpecification request;
+
+    // Feature: Consultar todos los empleados
+
     @Given("^un administrador quiere consultar empleados$")
     public void ruta_de_API() throws Exception {
-        RestAssured.port  = PORT;
+        RestAssured.port = PORT;
         request = RestAssured.given();
 
     }
-    
+
     Response response;
+
     @When("realiza petición de consulta")
     public void call_getAll_API() throws Exception {
-    	response = request.get("/get");
+        response = request.get("/get");
 
     }
 
     @Then("^devuelve un json$")
     public void status_200_y_body_tipo_json() throws Exception {
-    	int statusCode = response.getStatusCode();
-    	Assert.assertEquals(200,statusCode);
-    	JSONObject json = new JSONObject(response.body().asString());
-    	assertTrue(json instanceof JSONObject);
+        int statusCode = response.getStatusCode();
+        Assert.assertEquals(200, statusCode);
+        JSONObject json = new JSONObject(response.body().asString());
+        assertTrue(json instanceof JSONObject);
 
     }
 
     int lengthExpected;
-    
+
     @But("^la lista está vacía$")
     public void la_lista_está_vacía() throws Exception {
-    	lengthExpected = 0;
-    	
+        lengthExpected = 0;
+
     }
 
     @Then("^devuelve advertencia$")
     public void devuelve_advertencia() throws Exception {
-    int responseLenght = response.body().toString().length();
-    Assert.assertEquals(lengthExpected, responseLenght);
+        int responseLenght = response.body().toString().length();
+        Assert.assertEquals(lengthExpected, responseLenght);
 
     }
-    
-    
+
     int itemsExpected;
-    
-    @And("conoce numero exacto empleados") 
+
+    @And("conoce numero exacto empleados")
     public void la_lista_tiene_10_empleados() throws Exception {
-    	itemsExpected = 10;
-    	
+        itemsExpected = 10;
+
     }
-    
+
     @Then("devuelve json con número exacto empleados")
     public void devuelve_json_10_items() throws Exception {
         List<E> listaEmpleados = response.body().jsonPath().getList("empleados");
         Assert.assertEquals(itemsExpected, listaEmpleados.size());
-        
+
     }
-    
+
     int statusCodeExpected;
-    
+
     @But("^no existe el archivo$")
     public void no_existe_el_archivo() throws Exception {
-    	statusCodeExpected = 404;
-    	
+        statusCodeExpected = 404;
+
     }
 
     @Then("^mostrar error de advertencia$")
     public void mostrar_error_de_advertencia() throws Exception {
-    int statusCode = response.getStatusCode();
-    Assert.assertEquals(statusCodeExpected, statusCode);
-    
+        int statusCode = response.getStatusCode();
+        Assert.assertEquals(statusCodeExpected, statusCode);
+
     }
-    
-    //Feature: Buscar datos detallados de un empleado
-    
+
+    // Feature: Buscar datos detallados de un empleado
+
     @Given("un administrador quiere consultar un empleado$")
     public void ruta_de_API2() throws Exception {
-        RestAssured.port  = PORT;
+        RestAssured.port = PORT;
         request = RestAssured.given();
 
     }
-    
+
     @When("realiza petición de consulta por nombre de usuario")
     public void call_getEmpleado_API() throws Exception {
-    	String usuarioEmpleado = "jeserodiguez93";
-    	response = request.get("/get/"+usuarioEmpleado);
+        String usuarioEmpleado = "jeserodiguez93";
+        response = request.get("/get/" + usuarioEmpleado);
 
     }
-    
+
     @Then("^devuelve un json$")
     public void status_200() throws Exception {
-    	int statusCode = response.getStatusCode();
-    	Assert.assertEquals(200,statusCode);
+        int statusCode = response.getStatusCode();
+        Assert.assertEquals(200, statusCode);
+
+    }
+
+    // Scenario: Da error si al pedir los datos de un empleado, el valor de alguna
+    // propiedad está vacio
+    int propiedadCodeExpected;
+
+    @But("algunas de sus propiedades están vacías")
+    public void alguna_propiedad_vacia() throws Exception {
+        propiedadCodeExpected = 0;
+    }
+
+    @Then("^devuelve un error$")
+    public void devolver_error_con_propiedad_vacia() throws Exception {
+        apellidoLenght = request.get();
 
     }
 }
-
-
