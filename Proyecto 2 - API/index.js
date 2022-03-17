8b1d766b52cad672dcdacd7945e14ebd5c6f26e8
xn0 <chanomm75@gmail.com>
2022-03-17 19:12:29 +0100

    Hosting API Heroku

--- Proyecto 2 - API/index.js
+++ Proyecto 2 - API/index.js
@@ -1,21 +1,32 @@
 var express = require("express");
+const res = require("express/lib/response");
 var app = express();
+var hbs = require('hbs');
+const port = process.env.PORT || 3001;
+const res1 = require('dotenv').config();
+
+var listado = __dirname + '/db.json';
+
+const fs = require("fs");
+
+app.set('view engine', 'hbs');
 
 
-app.get('/get', function (req, res) {
-    res.sendFile(__dirname + '/db.json');
+app.get('/empleados', function (req, res) {
+    if (fs.existsSync(listado)) res.sendFile(listado);
+
+    else res.status(404).send("No se encuentra el listado");
 });
 
 /* app.get('get/{user}', function (req, res) {
     
 }); */
 
-var server = app.listen(8080, function() {
-
-    var port = server.address().port;
+var server = app.listen(port, function() {
 
     console.log("Escuchando por el puerto " + port + ".");
 
 })
 
 module.exports = server;
+
