891969c7cacc7567e2a0b7dc0c9bc34943fed480
Vieris <alex12293@gmail.com>
2022-03-17 17:08:31 +0100

    errores

--- Proyecto 2 - API/index.js
+++ Proyecto 2 - API/index.js
@@ -1,14 +1,17 @@
 var express = require("express");
 var app = express();
 
+var listado = __dirname + '/db.json';
+const fs = require("fs");
 
 app.get('/get', function (req, res) {
-    res.sendFile(__dirname + '/db.json');
+    if (fs.existsSync(listado)) res.sendFile(listado);
+    else res.status(404).send("No se encuentra el listado");
 });
 
 app.get('/empleado/:usuario', function(req, res) {
     const usuario = req.params.usuario;
-    var empleados = require(__dirname + '/db.json').empleados;
+    var empleados = require(listado).empleados;
     var datos = empleados.find(x => x.usuario == usuario);
     
     if (JSON.stringify(datos)) res.send(datos);
